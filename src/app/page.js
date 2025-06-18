56c404773d6ec7bb4ab709c4d405d6473f9646a5
renesalomone <108975981+renesalomone@users.noreply.github.com>
2025-06-18 22:19:36 +0700

    added image to home page

--- src/app/page.js
+++ src/app/page.js
@@ -2,15 +2,29 @@ import Image from 'next/image'
 
 export default function Home() {
   return (
-    <>
-      <h1 className='text-4xl text-semibold'>Rene Maxey-Salomone</h1>
-      <h2 className='text-2xl mt-2'>Full-Stack Software Engineer</h2>
+    <div className='grid grid-cols-1 md:grid-cols-2 gap-8 items-start'>
+      {/* Text Column */}
+      <div className=''>
+        <h1 className='text-4xl text-semibold'>Rene Maxey-Salomone</h1>
+        <h2 className='text-2xl mt-2'>Full-Stack Software Engineer</h2>
 
-      <p className='max-w-xl pt-10'>
-        I am a software engineer looking for work. I specialize in building websites. Lorem ipsum
-        dolor sit amet, consectetur adipiscing elit. Maecenas viverra lobortis metus, nec ultricies
-        sem mollis quis. In sed libero condimentum, facilisis turpis ut, placerat risus.
-      </p>
-    </>
+        <p className='max-w-xl pt-10'>
+          I am a software engineer looking for work. I specialize in building websites. Lorem ipsum
+          dolor sit amet, consectetur adipiscing elit. Maecenas viverra lobortis metus, nec
+          ultricies sem mollis quis. In sed libero condimentum, facilisis turpis ut, placerat risus.
+        </p>
+      </div>
+
+      {/* Image Column */}
+      <div className='flex justify-center'>
+        <Image
+          src='/profile1.jpg'
+          alt='Rene Maxey-Salomone'
+          width={300}
+          height={0}
+          layout='intrinsic'
+        />
+      </div>
+    </div>
   )
 }
