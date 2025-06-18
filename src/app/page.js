8385cfa9bf17ec716285bbe4e230c64a7959da05
renesalomone <108975981+renesalomone@users.noreply.github.com>
2025-06-18 22:28:57 +0700

    UI updates for home page

--- src/app/page.js
+++ src/app/page.js
@@ -5,14 +5,15 @@ export default function Home() {
     <div className='grid grid-cols-1 md:grid-cols-2 gap-8 items-start'>
       {/* Text Column */}
       <div className=''>
-        <h1 className='text-4xl text-semibold'>Rene Maxey-Salomone</h1>
-        <h2 className='text-2xl mt-2'>Full-Stack Software Engineer</h2>
+        <h1 className='text-4xl font-semibold'>Rene Maxey-Salomone</h1>
+        <h2 className='text-2xl mt-2 font-semibold'>Full-Stack Software Engineer</h2>
 
-        <p className='max-w-xl pt-10'>
+        <p className='max-w-md pt-10'>
           I am a software engineer looking for work. I specialize in building websites. Lorem ipsum
           dolor sit amet, consectetur adipiscing elit. Maecenas viverra lobortis metus, nec
           ultricies sem mollis quis. In sed libero condimentum, facilisis turpis ut, placerat risus.
         </p>
+        <div></div>
       </div>
 
       {/* Image Column */}
@@ -20,7 +21,7 @@ export default function Home() {
         <Image
           src='/profile1.jpg'
           alt='Rene Maxey-Salomone'
-          width={300}
+          width={250}
           height={0}
           layout='intrinsic'
         />
