8d77128c002c7b352048fc44ca537efbef03334e
renesalomone <108975981+renesalomone@users.noreply.github.com>
2025-07-04 15:04:13 +0700

    added logo to navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -17,40 +17,47 @@ export default function Navbar() {
   return (
     <nav
       className={cn(
-        'hidden md:flex gap-10 fixed top-0 left-0 w-full justify-center items-center h-14 text-md text-foreground z-50 backdrop-blur bg-[linear-gradient(to_right,#0f172a,#4338ca)]'
+        'hidden md:flex fixed top-0 left-0 w-full justify-center items-center h-14 text-md text-foreground z-50 backdrop-blur bg-[linear-gradient(to_right,#0f172a,#4338ca)]'
       )}
     >
-      <FaSun className='text-white mr-auto ml-20' />
-
-      {navItems.map((item) => (
-        <NavLink
-          key={item.href}
-          href={item.href}
-          className={cn(
-            'text-center text-white inline-block transition-all font-semibold hover:underline underline-offset-6 decoration-2',
-            pathname === item.href && 'underline underline-offset-6 decoration-2'
-          )}
+      <a href='/'>
+        <img
+          src='/logo.svg'
+          alt='logo'
+          className='h-4 w-auto ml-10 mr-16'
+        />
+      </a>
+      <div className='flex gap-6 mx-auto'>
+        {navItems.map((item) => (
+          <NavLink
+            key={item.href}
+            href={item.href}
+            className={cn(
+              'text-center text-white inline-block transition-all font-semibold hover:underline underline-offset-6 decoration-2',
+              pathname === item.href && 'underline underline-offset-6 decoration-2'
+            )}
+          >
+            {item.label}
+          </NavLink>
+        ))}
+        <a
+          href='/resume.pdf'
+          target='_blank'
+          rel='noopener noreferrer'
+          className='text-white font-semibold hover:text-rose-600'
         >
-          {item.label}
-        </NavLink>
-      ))}
-      <a
-        href='/resume.pdf'
-        target='_blank'
-        rel='noopener noreferrer'
-        className='text-white font-semibold hover:underline hover:underline-offset-4 decoration-2'
-      >
-        Resume
-      </a>
-      <a
-        href='https://github.com/ReneMSdev'
-        target='_blank'
-        rel='noopener noreferrer'
-        className='text-white font-semibold hover:underline hover:underline-offset-4 decoration-2'
-      >
-        Github
-      </a>
-      <FaMoon className='text-white ml-auto mr-20' />
+          Resume
+        </a>
+        <a
+          href='https://github.com/ReneMSdev'
+          target='_blank'
+          rel='noopener noreferrer'
+          className='text-white font-semibold hover:text-rose-600'
+        >
+          Github
+        </a>
+      </div>
+      <FaMoon className='text-white ml-auto mr-10 cursor-pointer' />
     </nav>
   )
 }
